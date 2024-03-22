9ec20bb0b23fbfbe1e6bda51b0d5e76acbb2d85a
kamruzzaman1234 <dmkkaarr.labib980@gmail.com>
2024-03-22 13:52:52 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h3 className="text-3xl bg-red-400">Bangladesh</h3> 
+    <div className="avatar">
+  <div className="w-32 rounded">
+    <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+  </div>
+</div>
+<div className="avatar">
+  <div className="w-20 rounded">
+    <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" alt="Tailwind-CSS-Avatar-component" />
+  </div>
+</div>
+<div className="avatar">
+  <div className="w-16 rounded">
+    <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" alt="Tailwind-CSS-Avatar-component" />
+  </div>
+</div>
+<div className="avatar">
+  <div className="w-8 rounded">
+    <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" alt="Tailwind-CSS-Avatar-component" />
+  </div>
+</div>
+     
+    </>
+  )
+}
+
+export default App
